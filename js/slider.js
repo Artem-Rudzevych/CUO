2e5f5e3e5b5fafbd339be5c94849290e884b5815
Artem-Rudzevych <mrbig8644@gmail.com>
2022-07-19 18:29:09 +0300

    Добавил слайдер героя

---
+++ js/slider.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // инициализация слайдера
+  var slider = new ItcSimpleSlider(".itcss", {
+    loop: false,
+    autoplay: true,
+    swipe: true,
+  });
+});
