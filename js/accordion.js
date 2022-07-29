5f1af6955587774ec7b0cf933f8cd70ea0e60876
Artem-Rudzevych <mrbig8644@gmail.com>
2022-07-29 09:27:22 +0300

    Настроил акордион

--- js/accordion.js
+++ js/accordion.js
@@ -1,18 +1,14 @@
-const boxes = Array.from(document.querySelectorAll(".box"));
+const accordion = document.querySelectorAll(".box__item");
 
-boxes.forEach((box) => {
-  box.addEventListener("click", boxHandler);
-});
-
-function boxHandler(e) {
-  e.preventDefault();
-  let currentBox = e.target.closest(".box");
-  let currentContent = e.target.nextElementSibling;
-  currentBox.classList.toggle("active");
-
-  if (currentBox.classList.contains("active")) {
-    currentContent.style.maxHeight = currentContent.scrollHeight + "px";
-  } else {
-    currentContent.style.maxHeight = 0;
-  }
+for (item of accordion) {
+  item.addEventListener("click", function () {
+    if (this.classList.contains("active")) {
+      this.classList.remove("active");
+    } else {
+      for (el of accordion) {
+        el.classList.remove("active");
+      }
+      this.classList.add("active");
+    }
+  });
 }
